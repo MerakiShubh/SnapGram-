f3a3eef7bac3984c2385aa32c20a4bbf31e83762
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-27 22:46:30 +0530

    code under review

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -70,6 +70,7 @@ export async function getCurrentUser() {
       appwriteConfig.userCollectionId,
       [Query.equal("accountId", currentAccount.$id)]
     );
+    if(!currentUser) throw Error;
     return currentUser.documents[0];
   } catch (error) {
     console.log(error);
