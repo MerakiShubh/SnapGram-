0e5328b751852b5f1c7d1fe6c4a091fee9630d28
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-28 23:20:51 +0530

    bug fix

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,16 +1,21 @@
-import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from "@tanstack/react-query";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
 import { createUserAccount } from "../appwrite/api";
 import { INewUser } from "@/types";
 import { signInAccount } from "../appwrite/api";
 
 export const useCreateUserAccount = () => {
-    return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user)
-    })
-
-}
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
 export const useSingInAccount = () => {
-    return useMutation({
-        mutationFn: (user: {email: string, password: string}) => signInAccount(user)
-    })
-}
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
